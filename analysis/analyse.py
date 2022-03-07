63f4aaca6c20e55682a03213b3ca76f90cb73aa0
templier1994 <amos.jerome1994@gmail.com>
2022-03-07 11:30:38 +0100

    add some test at start of functions

--- analysis/analyse.py
+++ analysis/analyse.py
@@ -1,21 +1,27 @@
 from sklearn import metrics
-from sklearn.metrics import confusion_matrix
 
 ####
 # return mean absolute error
 ####
 def MAE(y_test, y_pred):
+    if(y_test.shape != y_pred.shape):
+        raise Exception("Analyse : MAE, not same shape")
+
     return metrics.mean_absolute_error(y_test,y_pred)
 
 ####
 # return r2 score
 ####
 def r2(y_test, y_pred):
+    if(y_test.shape != y_pred.shape):
+        raise Exception("Analyse : r2 score, not same shape")
     return metrics.r2_score(y_test,y_pred)
 ####
 # return root mean squared error
 ####
 def rmse(y_test, y_pred):
+    if(y_test.shape != y_pred.shape):
+        raise Exception("Analyse : rmse, not same shape")
     return metrics.mean_squared_error(y_test,y_pred)
 
 
