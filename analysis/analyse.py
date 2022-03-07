da8eb5e812b14169ae372d815c0e600ee5b8f197
templier1994 <amos.jerome1994@gmail.com>
2022-03-07 11:27:53 +0100

    comment analysis and clean code

--- analysis/analyse.py
+++ analysis/analyse.py
@@ -1,21 +1,21 @@
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix
-import matplotlib.pyplot as plt
 
+####
+# return mean absolute error
+####
 def MAE(y_test, y_pred):
     return metrics.mean_absolute_error(y_test,y_pred)
 
-#r2 score
+####
+# return r2 score
+####
 def r2(y_test, y_pred):
     return metrics.r2_score(y_test,y_pred)
-
-# root mean squared error
+####
+# return root mean squared error
+####
 def rmse(y_test, y_pred):
     return metrics.mean_squared_error(y_test,y_pred)
 
-
-def conf_mat(y_test, y_pred):
-
-    results = confusion_matrix(y_test, y_pred)
-    return results
 
