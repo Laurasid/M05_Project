b51c3d652141d6024cd0ee628205eee53e9e51f1
templier1994 <amos.jerome1994@gmail.com>
2022-03-21 14:25:25 +0100

    comment and document code

--- global_test.py
+++ global_test.py
@@ -5,20 +5,29 @@ from analysis import analyse
 
 
 def run_all_possibilities():
+    """
+    Function that run all possibilities of dataset-split-scaling-algorithms
+
+    :return: returnArray
+         return an array of string containing all values
+    """
     returnArray = []
     datasets = []
 
+    #import the 3 datasets
     dataset1 = pp.import_dataset("Data/winequality-red.csv")
     dataset2 = pp.import_dataset("Data/winequality-white.csv")
     dataset3 = pp.import_dataset("Data/housing.data")
 
+    #put datasets into an array
     datasets.append(("red_Wine",dataset1))
     datasets.append(("white_wine",dataset2))
     datasets.append(("housing",dataset3))
 
+    #loop on all possibilities of mix dataset-split-scaling-algorithms
     for dataset in datasets:
         for seed in range(1,4): #1-2-3
-            for norm in range(1,5):
+            for norm in range(1,5):#1-2-3-4
                 #switch(seed):
                 x_train, x_test, y_train, y_test = pp.preprocessing(dataset[1], seed, norm)
 
@@ -41,11 +50,19 @@ def run_all_possibilities():
                                    str(r2_lr) + ", rmse : " + str(rmse_lr) + "\n")
                 returnArray.append("Regression tree   : dataset : " + dataset[0] + ", seed : " + str(seed) + ", norm : " + str(norm) +", mae : " + str(mae_rt) + ", r2 :" +
                                    str(r2_rt) + ", rmse : " + str(rmse_rt) + "\n")
-
-
     return returnArray
 
+
 def save_into_file(fileName, data):
+    """
+    Save values into a file to have a better view a check reproducibility of code
+
+    :param fileName: Str
+
+    :param data: array of string containing all values
+
+    :return:
+    """
     f = open(fileName,"w+")
     for i in data:
         f.write(i)
