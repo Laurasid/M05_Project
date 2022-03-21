68ceb6ac0ea6bccd35d7e7f3734c7a22734a6043
templier1994 <amos.jerome1994@gmail.com>
2022-03-21 08:45:25 +0100

    make documentation for analysis and linear_regresion

--- analysis/analyse.py
+++ analysis/analyse.py
@@ -6,6 +6,15 @@ import matplotlib.pyplot as plt
 # return mean absolute error
 ####
 def mae(y_test, y_pred):
+    """
+    Function to calculate the mean aboslute error with sklearn lib
+
+    :param y_test:
+
+    :param y_pred:
+
+    :return: mea
+    """
     if y_test.shape != y_pred.shape:
         raise Exception("Analyse : MAE, not same shape")
 
@@ -16,6 +25,15 @@ def mae(y_test, y_pred):
 # return r2 score
 ####
 def r2(y_test, y_pred):
+    """
+    Function to calculate the r2 with sklearn lib
+
+    :param y_test:
+
+    :param y_pred:
+
+    :return: r2
+    """
     if y_test.shape != y_pred.shape:
         raise Exception("Analyse : r2 score, not same shape")
     return metrics.r2_score(y_test, y_pred)
@@ -25,6 +43,15 @@ def r2(y_test, y_pred):
 # return root mean squared error
 ####
 def rmse(y_test, y_pred):
+    """
+    Function to calculate the root mean squarred error with sklearn lib
+
+    :param y_test:
+
+    :param y_pred:
+
+    :return: rmse
+    """
     if y_test.shape != y_pred.shape:
         raise Exception("Analyse : rmse, not same shape")
     return metrics.mean_squared_error(y_test, y_pred)
@@ -34,6 +61,14 @@ def rmse(y_test, y_pred):
 # Plot a correlation matrix
 ####
 def correlation_matrix(dataset):
+    """
+    Function to create a correlation_matrix with seaborn lib, save it as png into analysis folder
+
+    :param dataset:
+        Pandas dataframe
+
+    :return:
+    """
     fig = plt.figure(figsize=[15, 10])
     sns.heatmap(dataset.corr())
     plt.title("Correlation matrix")
--- model/linear_regression.py
+++ model/linear_regression.py
@@ -2,11 +2,28 @@ from sklearn.linear_model import LinearRegression
 
 
 def train (x_train, y_train):
+    """
+    Function to train an linear regression model
+    :param x_train:
+    :param y_train:
+    :return: regressor
+        sklearn regressor of the linear model
+    """
     regressor = LinearRegression()
     regressor.fit(x_train, y_train)
 
     return regressor
 
 def predict (regressor, x_test) :
+    """
+    Function to make a prediction on a set of data
+
+    :param regressor:
+
+    :param x_test:
+
+    :return: y_pred
+        Predicted Value
+    """
     y_pred = regressor.predict(x_test)
     return y_pred
