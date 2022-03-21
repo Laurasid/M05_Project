accd08810ec87d08b7c444b303df21a7005b3f50
Laura Sidler <sidler@MacBook-Pro-de-Laura.local>
2022-03-21 22:00:44 +0100

    update file for auto documentation

--- model/regression_tree.py
+++ model/regression_tree.py
@@ -1,25 +1,31 @@
 from sklearn.tree import DecisionTreeRegressor
 
-"""
-Function : regression_tree::train
-Param : x_train : values of train dataset
-        y_train : labels of train dataset
-Return : regressor : DecisionTreeRegressor object
-"""
+###
+# train the model
+###
 def train(x_train, y_train):
+    """
+    Function to train a regression tree model
+    :param array-like x_train: train values
+    :param array-like y_train: train labels
+    :return: DecisionTreeRegressor regressor: sklearn regressor of the linear model
+    """
     regressor = DecisionTreeRegressor()
     regressor.fit(x_train, y_train)
 
     return regressor
 
 
-"""
-Function : regression_tree::predict
-Param : regressor : DecisionTreeRegressor object returned by train method
-        x_test : values of test dataset
-Return : y_pred : labels predicted for the values x_test
-"""
+###
+# predict likely values
+###
 def predict(regressor, x_test):
+    """
+    Function to train a regression tree model
+    :param DecisionTreeRegressor regressor: regressor
+    :param array-like x_test: test values
+    :return: array-like y_pred: predicted values
+    """
     y_pred = regressor.predict(x_test)
 
     return y_pred
