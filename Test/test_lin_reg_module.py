b49b16f0bccda9e4c9e74ee1cc543d3f5a2be3e4
Laura Sidler <sidler@MacBook-Pro-de-Laura.local>
2022-03-21 23:00:46 +0100

    update for auto documentation

--- Test/test_lin_reg_module.py
+++ Test/test_lin_reg_module.py
@@ -6,6 +6,15 @@ import numpy as np
 import random
 
 def test_lin_reg():
+    """
+    Function to test linear regression module. Test for shape and type of values returned.
+
+    :param None
+
+    :return: None
+
+    :raise: AssertionError
+    """
     x_train = np.array([np.random.rand(2),
                         np.random.rand(2)])
     y_train = np.array([np.random.rand(2),
@@ -16,5 +25,6 @@ def test_lin_reg():
     regressor = lr.train(x_train,y_train)
     y_pred = lr.predict(regressor,x_test)
 
-    # test shape of the model's output value
+    # test shape and type of the model's output value
     assert np.shape(y_pred) == np.shape(x_test)
+    assert isinstance(y_pred,type(x_test))
