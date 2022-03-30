e137de92d60b12c644fbbb0c7b603b68e7b97dce
Laura Sidler <sidler@MacBook-Pro-de-Laura.local>
2022-03-30 14:24:25 +0200

    add another path

--- test/test_analysis_module.py
+++ test/test_analysis_module.py
@@ -1,7 +1,9 @@
 import pytest
 import sys
 import os
-sys.path.append(os.path.dirname(sys.path[0]))
+#sys.path.append(os.path.dirname(sys.path[0]))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, BASE_DIR)
 
 sys.path.append('../analysis')
 import analyse as an
--- test/test_lin_reg_module.py
+++ test/test_lin_reg_module.py
@@ -1,7 +1,9 @@
 import pytest
 import sys
 import os
-sys.path.append(os.path.dirname(sys.path[0]))
+#sys.path.append(os.path.dirname(sys.path[0]))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, BASE_DIR)
 
 sys.path.append('../model')
 import linear_regression as lr
