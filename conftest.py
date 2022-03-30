ed3a7ff938b9870522ec43f2fab8328f2453e986
Laura Sidler <sidler@MacBook-Pro-de-Laura.local>
2022-03-30 14:37:21 +0200

    the trial of despair

--- conftest.py
+++ conftest.py
@@ -1,8 +1,1 @@
-import os
-import sys
-# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '. ')))
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, BASE_DIR)
-
-#sys.path.append(os.path.dirname(sys.path[0]))
