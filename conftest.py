b5c2071f8cd08b35364d63fbe9cd0dd83987e778
Laura Sidler <sidler@MacBook-Pro-de-Laura.local>
2022-03-30 14:28:56 +0200

    modify conftest.py

--- conftest.py
+++ conftest.py
@@ -1,4 +1,8 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '. ')))
-sys.path.append(os.path.dirname(sys.path[0]))
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, BASE_DIR)
+
+#sys.path.append(os.path.dirname(sys.path[0]))
--- test/test_reg_tree_module.py
+++ test/test_reg_tree_module.py
@@ -1,10 +1,5 @@
 import pytest
 import sys
-import os
-#sys.path.append(os.path.dirname(sys.path[0]))
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, BASE_DIR)
-
 sys.path.append('../model')
 import regression_tree as rt
 import numpy as np
